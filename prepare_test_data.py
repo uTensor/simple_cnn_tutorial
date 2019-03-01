acfb259cca77de38243d89570aa18dc2e6e7f753
dboyliao <qmalliao@gmail.com>
2019-03-02 01:15:52 +0800

    setup mbed

--- prepare_test_data.py
+++ prepare_test_data.py
@@ -1,30 +1,41 @@
 import os
 from random import random
 
+import click
 import idx2numpy
 import torch
 from torchvision import transforms
 from torchvision.datasets import cifar
-from utensor_cgen.utils import save_idx
-
-cifar10_train = cifar.CIFAR10("./cifar10_data", download=True, train=True)
-cifar10_test = cifar.CIFAR10("./cifar10_data", download=True, train=False)
 
 
-mean = (cifar10_train.train_data.astype("float32") / 255.0).mean(axis=(0, 1, 2))
-std = (cifar10_train.train_data.astype("float32") / 255.0).std(axis=(1, 2)).mean(axis=0)
+@click.command()
+@click.help_option('-h', '--help')
+@click.argument('target_label', default=3, type=int)
+def prepare_test_data(target_label):
+    cifar10_train = cifar.CIFAR10("./cifar10_data", download=True, train=True)
+    mean = (cifar10_train.train_data.astype("float32") / 255.0).mean(axis=(0, 1, 2))
+    std = (cifar10_train.train_data.astype("float32") / 255.0).std(axis=(1, 2)).mean(axis=0)
+    done = False
+    while not done:
+        for label, data in zip(cifar10_train.train_labels, cifar10_train.train_data):
+            if label == target_label and random() < 0.1:
+                data = data.astype('float32') / 255.0
+                norm_data = (data - mean) / std
+                with open('img_data.h', 'w') as fid:
+                    fid.write(
+                        '#ifndef __IMG_DATA_H\n'
+                        '#define __IMG_DATA_H\n'
+                    )
+                    fid.write('static const int label_true = {};\n'.format(target_label))
+                    fid.write(
+                        'static const float img_data[{}] = {};\n'.format(
+                            len(norm_data.ravel()),
+                            str(norm_data.ravel().tolist()).replace('[', '{\n').replace(']', '\n}')
+                        )
+                    ) 
+                    fid.write('#endif // __IMG_DATA_H\n')
+                    done = True
+                    break
 
-
-idx_map = {}
-while len(idx_map) < 10:
-    for i, data in enumerate(cifar10_train.train_data):
-        label = cifar10_train.train_labels[i]
-        if random() < 0.1 and label not in idx_map:
-            idx_map[label] = i
-
-if not os.path.exists("idx_data"):
-    os.mkdir("idx_data")
-for label, idx in idx_map.items():
-    data = cifar10_train.train_data[idx].astype("float32") / 255.0
-    norm_data = (data - mean) / std
-    save_idx(norm_data, "idx_data/{}.idx".format(label))
+if __name__ == '__main__':
+    prepare_test_data()
