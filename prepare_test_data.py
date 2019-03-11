1e6fb5dc34267662b54874b64c05b1f8d45f6ecb
dboyliao <qmalliao@gmail.com>
2019-03-11 22:56:09 +0800

    update preparing data script

--- prepare_test_data.py
+++ prepare_test_data.py
@@ -2,40 +2,57 @@ import os
 from random import random
 
 import click
-import idx2numpy
-import torch
-from torchvision import transforms
+
 from torchvision.datasets import cifar
+
+
+def _random_sample(accept_prob):
+    cifar10_train = cifar.CIFAR10("./cifar10_data", download=True, train=False)
+    img_data = cifar10_train.test_data
+    labels = cifar10_train.test_labels
+    mean = (img_data.astype("float32") / 255.0).mean(axis=(0, 1, 2))
+    std = (img_data.astype("float32") / 255.0).std(axis=(1, 2)).mean(axis=0)
+    label_data_map = {}
+    while len(label_data_map) < 10:
+        for label, data in zip(labels, img_data):
+            if label not in label_data_map and random() < accept_prob:
+                norm_data = (data.astype('float32') / 255.0 - mean) / std
+                label_data_map[label] = norm_data.ravel()
+    return label_data_map
 
 
 @click.command()
 @click.help_option('-h', '--help')
-@click.argument('target_label', default=3, type=int)
-def prepare_test_data(target_label):
-    cifar10_train = cifar.CIFAR10("./cifar10_data", download=True, train=True)
-    mean = (cifar10_train.train_data.astype("float32") / 255.0).mean(axis=(0, 1, 2))
-    std = (cifar10_train.train_data.astype("float32") / 255.0).std(axis=(1, 2)).mean(axis=0)
-    done = False
-    while not done:
-        for label, data in zip(cifar10_train.train_labels, cifar10_train.train_data):
-            if label == target_label and random() < 0.1:
-                data = data.astype('float32') / 255.0
-                norm_data = (data - mean) / std
-                with open('img_data.h', 'w') as fid:
-                    fid.write(
-                        '#ifndef __IMG_DATA_H\n'
-                        '#define __IMG_DATA_H\n'
-                    )
-                    fid.write('static const int label_true = {};\n'.format(target_label))
-                    fid.write(
-                        'static const float img_data[{}] = {};\n'.format(
-                            len(norm_data.ravel()),
-                            str(norm_data.ravel().tolist()).replace('[', '{\n').replace(']', '\n}')
-                        )
-                    ) 
-                    fid.write('#endif // __IMG_DATA_H\n')
-                    done = True
-                    break
+@click.option('--accept-prob', default=0.3, show_default=True)
+@click.option('--labels', multiple=True, show_default=True,
+              type=int, help='labels to add to testing data')
+def prepare_test_data(accept_prob, labels):
+    labels = list(labels) or [i for i in range(10)]
+    data_map = _random_sample(accept_prob)
+    with open('img_data.h', 'w') as fid:
+        fid.write(
+            '#ifndef __IMG_DATA_H\n'
+            '#define __IMG_DATA_H\n'
+        )
+        num_elems = data_map[0].shape[0]
+        fid.write(
+            'static const float imgs_data[%s][%s] = {\n' % (len(labels), num_elems)
+        )
+        for label in labels:
+            data = data_map[label]
+            fid.write('    {\n')
+            buffer = []
+            for elem in data:
+                if len(buffer) == 5:
+                    fid.write('     ' + ', '.join(buffer) + ',\n')
+                    buffer = []
+                buffer.append('{:0.6f}'.format(elem))
+            if buffer:
+                fid.write('     ' + ', '.join(buffer) + ',\n')
+            fid.write('    },\n')
+        fid.write('};\n')
+        fid.write('#endif // __IMG_DATA_H\n')
+
 
 if __name__ == '__main__':
     prepare_test_data()
